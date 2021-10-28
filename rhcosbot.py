5e1510f83c5c7d250f14b527d35c23efac945ec6
Siddharth Sharma <sid.sharma0@gmail.com>
2021-10-28 16:25:18 -0400

    Propagate whiteboard field when backporting (#2)

--- rhcosbot.py
+++ rhcosbot.py
@@ -590,6 +590,7 @@ class CommandHandler(metaclass=Registry):
             'groups',
             'severity',
             'version',
+            'whiteboard'
         ])
         if bug.severity == 'unspecified':
             # Eric-Paris-bot will unset the target version without a severity
@@ -648,6 +649,7 @@ class CommandHandler(metaclass=Registry):
                     target_release=rel.target
                 )
                 info['cf_clone_of'] = cur_bug.id
+                info['whiteboard'] = bug.whiteboard
                 if need_bootimage:
                     info['cf_devel_whiteboard'] = self._bz.BOOTIMAGE_BUG_WHITEBOARD
                 bz = self._bz.api.createbug(info).id
